baf48cbe09d11dd92069eb8aed212163d1678880
TimurShakhlai <70841435+TimurShakhklai@users.noreply.github.com>
2020-10-25 21:30:54 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,37 +1,48 @@
-module.exports = function humanize(num){
-  var ones = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine',
-              'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen',
-              'seventeen', 'eighteen', 'nineteen'];
-  var tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty',
-              'ninety'];
+const NUMBERS = {
+    0: 'zero',
+    1: 'one',
+    2: 'two',
+    3: 'three',
+    4: 'four',
+    5: 'five',
+    6: 'six',
+    7: 'seven',
+    8: 'eight',
+    9: 'nine',
+    10: 'ten',
+    11: 'eleven',
+    12: 'twelve',
+    13: 'thirteen',
+    14: 'fourteen',
+    15: 'fifteen',
+    16: 'sixteen',
+    17: 'seventeen',
+    18: 'eighteen',
+    19: 'nineteen',
+    20: 'twenty',
+    30: 'thirty',
+    40: 'forty',
+    50: 'fifty',
+    60: 'sixty',
+    70: 'seventy',
+    80: 'eighty',
+    90: 'ninety',
+}
 
-  var numString = num.toString();
-
-  if (num < 0) throw new Error('Negative numbers are not supported.');
-
-  if (num === 0) return 'zero';
-
-  //the case of 1 - 20
-  if (num < 20) {
-    return ones[num];
-  }
-
-  if (numString.length === 2) {
-    return tens[numString[0]] + ' ' + ones[numString[1]];
-  }
-
-  //100 and more
-  if (numString.length == 3) {
-    if (numString[1] === '0' && numString[2] === '0')
-      return ones[numString[0]] + ' hundred';
-    else
-      return ones[numString[0]] + ' hundred ' + convert(+(numString[1] + numString[2]));
-  }
-
-  if (numString.length === 4) {
-    var end = +(numString[1] + numString[2] + numString[3]);
-    if (end === 0) return ones[numString[0]] + ' thousand';
-    if (end < 100) return ones[numString[0]] + ' thousand ' + convert(end);
-    return ones[numString[0]] + ' thousand ' + convert(end);
-  }
+module.exports = function toReadable(number) {
+    if (number <= 20){ 
+        return NUMBERS[number];
+    }else if (number > 20 && number < 100) {
+        if (number % 10 == 0){ 
+            return NUMBERS[number];
+        }else return `${NUMBERS[parseInt(number / 10) * 10]} ${NUMBERS[number - parseInt(number / 10) * 10]}`;
+    }
+    if (number >= 100) {
+        if (number % 100 == 0){ 
+            return `${NUMBERS[number / 100]} hundred`;
+        } else{
+            let dec = toReadable(number - parseInt(number / 100) * 100);
+            return `${NUMBERS[parseInt(number / 100)]} hundred ${dec}`;
+        }
+    }
 }
